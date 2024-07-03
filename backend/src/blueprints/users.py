d25a8c4f4653a87f909af38a7775c1ffda26d32b
Martin Gonzalez Figueira <magonzalezf@fi.uba.ar>
2024-07-03 15:35:39 -0300

    Modifico user. y users.py - Añado funcionalid de registro y login

--- backend/src/blueprints/users.py
+++ backend/src/blueprints/users.py
@@ -9,3 +9,22 @@ def list_all_users():
 
     users = UserService.get_all_users()
     return jsonify(users)
+
+@user_routes.route('/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+    new_user = UserService.register_user(name, email, password)
+    return jsonify(new_user), 201
+
+@user_routes.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    user = UserService.login_user(email, password)
+    if user:
+        return jsonify(user), 200
+    return jsonify({'message': 'Email o contraseña incorrectas'})
--- backend/src/services/user.py
+++ backend/src/services/user.py
@@ -1,8 +1,21 @@
 from models.models import User
 
-
 class UserService:
     @staticmethod
     def get_all_users():
         users = User.query.all()
         return [user.to_dict() for user in users]
+    
+    @staticmethod
+    def register_user(name, email, password):
+        user = User(name=name, email=email, password=password)
+        db.session.add()
+        db.session.commit()
+        return user.to_dict()
+    
+    @staticmethod
+    def login_user(email, password):
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            return user.to_dict()
+        return None
