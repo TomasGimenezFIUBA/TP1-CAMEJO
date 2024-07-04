154b37f24b1d40a01d3ee5c2c491ffe0c3bf54eb
Tomás Giménez <togimenez@fi.uba.ar>
2024-07-04 19:17:03 -0300

    junto los cambios de luis con los mios

--- frontend/src/config/routes.js
+++ frontend/src/config/routes.js
@@ -1,4 +1,8 @@
 export const publicRoutes = [
     "/login",
-    "/sign-up"
+    "/sign-up",
+    "/create-exercise",
+    "/"
 ]
+
+//TODO eliminar las rutas no publicas, esto es solo para develop
