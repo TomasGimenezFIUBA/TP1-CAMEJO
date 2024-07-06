0f69d3a9997fd05da562436e2b33fca9bfc12fb5
luis <lgavidia@fi.uba.ar>
2024-07-06 13:01:23 -0300

    diseños de las paginas para crear tus ejercicios y para ver los creados por las demas personas, ambas vinculadas al mismo filtro

--- frontend/src/config/routes.js
+++ frontend/src/config/routes.js
@@ -2,7 +2,9 @@ export const publicRoutes = [
     "/login",
     "/sign-up",
     "/create-exercise",
-    "/"
+    "/",
+    "/see-my-exercise",
+    "/search-exercise"
 ]
 
 //TODO eliminar las rutas no publicas, esto es solo para develop
