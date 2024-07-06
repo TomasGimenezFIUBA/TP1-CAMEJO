04c365232f1c9c9902361c229a4b0f7887d767fd
Martin Gonzalez Figueira <magonzalezf@fi.uba.ar>
2024-07-06 19:41:05 -0300

    Abm-ejercicios terminado

--- backend/src/blueprints/exercises.py
+++ backend/src/blueprints/exercises.py
@@ -5,16 +5,19 @@ from exceptions.ExercisesExceptions import ExerciseNotFoundException, InvalidExe
 
 exercise_routes = Blueprint('exercise_routes', __name__)
 
-@exercise_routes.route('/exercises', methods=['GET'])
+@exercise_routes.route('/exercises', methods=['GET']) #! Funciona
 def list_all_exercises():
     '''List all excercises'''
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 10))
+
     try:
-        exercises = ExerciseService.get_all_exercise() #TODO agregar paginacion
+        exercises = ExerciseService.get_all_exercises(page=page, per_page=per_page)
         return jsonify(exercises), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
-@exercise_routes.route('/exercises/<int:exercise_id>', methods=['GET'])
+@exercise_routes.route('/exercises/<int:exercise_id>', methods=['GET']) #! Funciona 
 def get_exercise_by_id(exercise_id):
     '''Get exercise by ID'''
     try:
@@ -26,7 +29,16 @@ def get_exercise_by_id(exercise_id):
     except Exception as e:
             return jsonify({'error': str(e)}), 500
     
-@exercise_routes.route('/exercises', methods=['POST'])
+@exercise_routes.route('/exercises/notuser/<int:user_id>', methods=['GET']) #! Funciona
+def get_notuser_exercises(user_id):
+    '''Get all exercises except from ID'''
+    try:
+        exercise = ExerciseService.get_notuser_exercises(user_id)
+        return jsonify(exercise), 200
+    except ExerciseNotFoundException as e:
+        return jsonify({'error': str(e)}), 404
+
+@exercise_routes.route('/exercises', methods=['POST']) #! Funciona
 def create_exercise():
     '''Create a new exercise'''
     try:
@@ -38,3 +50,33 @@ def create_exercise():
         return jsonify({'error': str(e)}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@exercise_routes.route('/exercises/<int:exercise_id>', methods=['PUT']) #! Funciona
+def update_exercise(exercise_id):
+    '''Update an existing exercise'''
+    try:
+        data = request.get_json()
+        user_id = data['user_id']  # Extraer el user_id del JSON del request
+        exercise = ExerciseService.update_exercise(exercise_id, user_id, data)
+        return jsonify(exercise), 200  # Código 200 para indicar éxito
+    except InvalidExerciseFormatException as e:  # Manejo de excepciones específicas
+        return jsonify({'error': str(e)}), 400
+    except TrainingEquipmenteNotFoundException as e:  # Manejo de excepciones específicas
+        return jsonify({'error': str(e)}), 404
+    except ExerciseNotFoundException as e:  # Manejo de excepciones específicas
+        return jsonify({'error': str(e)}), 404
+    except Exception as e:  # Manejo de cualquier otra excepción
+        return jsonify({'error': str(e)}), 500
+
+
+@exercise_routes.route('/exercises/<int:exercise_id>', methods=['DELETE']) #! Funciona a partir del ejercicio 4
+def delete_exercise(exercise_id):
+    '''Delete a exercise'''
+    try:
+        result = ExerciseService.delete_exercise(exercise_id)
+        if result:
+            return jsonify({'message': 'Exercise deleted'}), 200
+        else:
+            return jsonify({'error': 'Exercise not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500 
--- backend/src/services/exercise.py
+++ backend/src/services/exercise.py
@@ -10,8 +10,8 @@ class ExerciseService:
     available_muscles = ["bicep", 'tricep', 'shoulders', 'chest'] #atributo muscles="bicep;chest;"
 
     @staticmethod
-    def get_all_exercise():
-        exercises = Exercise.query.all()
+    def get_all_exercises(page, per_page):
+        exercises = Exercise.query.paginate(page=page, per_page=per_page)
         return [exercise.to_dict() for exercise in exercises]
     
     @staticmethod
@@ -29,7 +29,6 @@ class ExerciseService:
         exercise_muscles = data['muscles'].split(';')
         if not all(elem in ExerciseService.available_muscles for elem in exercise_muscles):
             raise InvalidExerciseFormatException(f'Muscles must be {ExerciseService.available_muscles}')
-
         
         user = UserService.get_user_by_id(user_id)
         if not user:
@@ -49,7 +48,61 @@ class ExerciseService:
             user_id = user_id,
             equipments = training_equipments
         )
-
+        
         db.session.add(exercise)
         db.session.commit()
         return exercise.to_dict()
+    
+    @staticmethod
+    def update_exercise(exercise_id, user_id, data):
+        if 'name' not in data or 'description' not in data or not user_id or 'muscles' not in data or 'equipments' not in data: #? ¿Esta verificación es necesaria?
+            raise InvalidExerciseFormatException(f'Required information is missing')
+        
+        exercise_muscles = data['muscles'].split(';')
+        if not all(elem in ExerciseService.available_muscles for elem in exercise_muscles):
+            raise InvalidExerciseFormatException(f'Muscles must be {ExerciseService.available_muscles}')
+
+        
+        user = UserService.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundException(f'User with id does not exists')
+
+        training_equipments = TrainingEquipmentServices.get_equipments_by_ids(data['equipments'])
+        if not training_equipments:
+            raise TrainingEquipmenteNotFoundException('At least one id is invalid')
+        exercise = Exercise.query.get(exercise_id)
+        if not exercise:
+            raise ExerciseNotFoundException(f'Exercise with id {exercise_id} not found')
+        
+        exercise.name = data['name']
+        exercise.description = data['description']
+        exercise.url = data['url']
+        exercise.calories = int(data['calories'])
+        exercise.extra_data = data['extra_data']
+        exercise.muscles = exercise_muscles
+        exercise.user_id = user_id
+        exercise.equipments = training_equipments   #? ¿Esto se termina modificando?
+
+
+        db.session.commit()
+        return exercise.to_dict()
+    
+    @staticmethod
+    def delete_exercise(exercise_id): #! ???
+        exercise = Exercise.query.filter_by(id=exercise_id).first()
+        
+        if not exercise:
+            raise ExerciseNotFoundException()
+
+        db.session.delete(exercise)    #? ¿Esto necesita estar dentro de un else? 
+        db.session.commit()         
+        return exercise.to_dict()
+    
+    @staticmethod 
+    def get_notuser_exercises(user_id): 
+
+        exercises = Exercise.query.filter(Exercise.user_id != user_id).all()
+        if not exercises:
+            raise ExerciseNotFoundException(f'Exercises without that id does not exists')
+        
+        return [exercise.to_dict() for exercise in exercises]
