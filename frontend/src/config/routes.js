c3bbbf794a47198f3e0ff80f49d85b49bc5c386d
Tomás Giménez <togimenez@fi.uba.ar>
2024-07-06 16:20:40 -0300

    muscle group selector en react

--- frontend/src/config/routes.js
+++ frontend/src/config/routes.js
@@ -2,7 +2,8 @@ export const publicRoutes = [
     "/login",
     "/sign-up",
     "/create-exercise",
-    "/"
+    "/",
+    "/demos"
 ]
 
 //TODO eliminar las rutas no publicas, esto es solo para develop
