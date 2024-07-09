624f185b0f9be9dc3f90931d5dea82ec23828e45
Tomás Giménez <togimenez@fi.uba.ar>
2024-07-09 17:51:29 -0300

    registrarse funciona raro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import viteConfig from './vite.config.js';
 
--- frontend/src/middleware/index.js
+++ frontend/src/middleware/index.js
@@ -6,13 +6,16 @@ async function validationRoute (context, next, request) {
     const currentURL = new URL(context.request.url)
     let isAuthenticated = context.request.headers.get('Cookie')?.includes('user_id')
 
+    console.log('Current URL:', currentURL.pathname)
     if (publicRoutes.includes(currentURL.pathname)) {
         return next()
     }
 
     if (!isAuthenticated) {
+        console.log('User is not authenticated')
+        
         return new Response(null, {
-            status: 401,
+            status: 302,
             headers: {
                 Location: '/login'
             }
